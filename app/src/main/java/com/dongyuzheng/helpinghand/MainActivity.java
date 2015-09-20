2e72412685c410013f35d7b8fd86e141e1c20d6e
Justine <justinedy096@gmail.com>
2015-09-19 20:14:11 -0400

    Fixed tab error

--- app/src/main/java/com/dongyuzheng/helpinghand/MainActivity.java
+++ app/src/main/java/com/dongyuzheng/helpinghand/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends TabActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tabHost =(TabHost) findViewById(R.id.tabhost);
         tabHost = getTabHost();
         tabHost.setup();
 
